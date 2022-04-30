1b36e4a0b68af0d7836a3b40100513e4486f1da4
pthouvenin <pierreantoine.thouvenin@gmail.com>
2022-04-30 12:04:41 +0200

    Build documentation as of 7ff953f611298c79a02c14e98910a30ff5b54a56

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0-66-g4cfadfa',
+    VERSION: '0-2-g7ff953f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
